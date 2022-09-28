f9518e17abbcae99b11d67bc92c9676c5ae9b4e2
Kayky Casagrande <kayky-cas@github.com>
2022-09-28 20:26:37 -0300

    2021/4 2/2

---
+++ 2021/4/1.cpp
@@ -0,0 +1,120 @@
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+void input_queue_to_int(list<int>*, string);
+
+int main(int argc, char const* argv[]) {
+  string file_name = "test.txt";
+
+  if (argc > 1)
+    file_name = argv[1];
+
+  fstream file_input(file_name);
+  if (!file_input.is_open())
+    return -1;
+
+  string input;
+
+  file_input >> input;
+
+  list<int> numbers;
+
+  input_queue_to_int(&numbers, input);
+
+  int num;
+
+  int tables[100][5][5];
+
+  int l = 0, c = 0, count = 0;
+
+  while (file_input >> num) {
+    if (l == 5) {
+      l = 0;
+      c++;
+    }
+
+    if (c == 5) {
+      c = 0;
+      count++;
+    }
+
+    tables[count][c][l++] = num;
+  }
+
+  int results[count + 1][5][5];
+
+  bool stop = false;
+
+  int last_number, winner_board;
+
+  for (int i = 0; i < count + 1; i++) {
+    for (int j = 0; j < 5; j++) {
+      for (int k = 0; k < 5; k++) {
+        results[i][j][k] = 0;
+      }
+    }
+  }
+
+  for (int actual : numbers) {
+    for (int i = 0; i < count + 1; i++) {
+      for (int j = 0; j < 5; j++) {
+        int line_count = 0;
+        int column_count = 0;
+        for (int k = 0; k < 5; k++) {
+          if (tables[i][j][k] == actual) {
+            results[i][j][k]++;
+          }
+
+          if (results[i][j][k] > 0)
+            line_count++;
+          if (results[i][k][j] > 0)
+            column_count++;
+        }
+        if (line_count == 5 || column_count == 5) {
+          last_number = actual;
+          winner_board = i;
+          stop = true;
+          break;
+        }
+      }
+      if (stop)
+        break;
+    }
+
+    if (stop)
+      break;
+  }
+
+  int sum = 0;
+
+  for (int j = 0; j < 5; j++) {
+    for (int k = 0; k < 5; k++) {
+      if (results[winner_board][j][k] == 0)
+        sum += tables[winner_board][j][k];
+    }
+  }
+
+  cout << last_number << endl;
+
+  cout << sum << endl;
+
+  cout << sum * last_number << endl;
+
+  return 0;
+}
+
+void input_queue_to_int(list<int>* n_list, string input) {
+  stringstream input_stream(input);
+
+  string number;
+
+  while (!input_stream.eof()) {
+    getline(input_stream, number, ',');
+    n_list->push_back(stoi(number));
+  }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # [Advent of Code](https://adventofcode.com)
 
 - [**2020**](https://adventofcode.com/2020): 1/50 :star:
-- [**2021**](https://adventofcode.com/2021): 7/50 :star:
+- [**2021**](https://adventofcode.com/2021): 8/50 :star:
